48a08a35c6a935ccef2e10a35b21effb4be16826
bk <120592075@qq.com>
2019-04-06 01:35:25 +0800

    190406.0135

--- bk_eureka/src/main/java/com/bk/eureka/BkEurekaApplication.java
+++ bk_eureka/src/main/java/com/bk/eureka/BkEurekaApplication.java
@@ -5,15 +5,29 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class BkEurekaApplication 
+@RestController
+public class BkEurekaApplication
 {
-	protected static final Logger logger = LoggerFactory.getLogger( BkEurekaApplication.class );
-	
-	public static void main(String[] args) {
+	protected static final Logger logger = LoggerFactory.getLogger(BkEurekaApplication.class);
+
+	public static void main(String[] args)
+	{
 		SpringApplication.run(BkEurekaApplication.class, args);
+//		new SpringApplicationBuilder(Application.class).web(true).run(args);
+	}
+	
+	/*
+	http://localhost:8000/bkEurekaHome
+	 */
+	@RequestMapping("/bkEurekaHome")
+	public String home()
+	{
+		return "bkEurekaHome, Hello World";
 	}
 
 }
