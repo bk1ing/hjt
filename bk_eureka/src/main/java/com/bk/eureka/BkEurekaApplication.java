f49ad783056e7e5fc52a61869cef30dd12740a57
bk <120592075@qq.com>
2019-04-05 12:35:58 +0800

    1235

---
+++ bk_eureka/src/main/java/com/bk/eureka/BkEurekaApplication.java
@@ -0,0 +1,19 @@
+package com.bk.eureka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class BkEurekaApplication 
+{
+	protected static final Logger logger = LoggerFactory.getLogger( BkEurekaApplication.class );
+	
+	public static void main(String[] args) {
+		SpringApplication.run(BkEurekaApplication.class, args);
+	}
+
+}
